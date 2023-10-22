10dd688bf62a2b6908cc5e951276a60c4e8e3e4e
Bigcat34 <146065179+Bigcat34@users.noreply.github.com>
2023-10-22 20:51:06 +0800

    Update hw3-1.c

--- hw3-1.c
+++ hw3-1.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+main()
+{
+int n,flag = 1;
+scanf("%d", &n);
+if (n > 1)
+{
+   for (int i = 2 ; i < n ; i++)
+   {
+       if (n%i == 0)
+       {
+           flag = 0;
+       }
+   }
 
+}
+else
+    flag = 0;
+if (flag == 1)
+    printf("%s","YES");
+else
+    printf("%s","NO");
+}
