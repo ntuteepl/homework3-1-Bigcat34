1bffdb69fef52b72a45baba3a3a3919357acdd41
Bigcat34 <146065179+Bigcat34@users.noreply.github.com>
2023-10-23 20:17:41 +0800

    Update hw3-2.c

--- hw3-2.c
+++ hw3-2.c
@@ -1,41 +1,35 @@
-#include <stdio.h>
 main()
 {
     int car = 3 ,s1,d1,s2,d2,s3,d3 ;
     scanf("%d %d %d %d %d %d",&s1,&d1,&s2,&d2,&s3,&d3);
     int slist[] = {s1,s2,s3};
     int dlist[] = {d1,d2,d3};
-     for (int i = 0 ; i < 3 ; i++)
-    {
-        if (car == 1)
-        {
-        break;
-        }
 
-        for (int j = 0 ; j < 3 ; j++)
-        {
-            if (car == 1)
-            {
-                break;
-            }
-            if (slist[j]>=dlist[i])
-            {
-                for (int k = 0 ; k < 3 ; k++)
-                {
-                   if (slist[k]>=dlist[j])
-                   {
-                       car = 1;
-                   }
-                    else
-                   {
-
-                       car = 2;
-                   }
-
-                }
-
+printf("%d %d %d %d %d %d\n",slist[0],dlist[0],slist[1],dlist[1],slist[2],dlist[2]);
+    void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+    for (int i = 0; i < 3 - 1; ++i) {
+        for (int j = 0; j < 3 - i - 1; ++j) {
+            if (slist[j] >slist[j + 1]) {
+                // 如果前一個數字比後一個大，交換它們
+                swap(&slist[j], &slist[j + 1]);
+                swap(&dlist[j], &dlist[j + 1]);
             }
         }
     }
+
+    if (dlist[0]>=slist[1] && dlist[1]<=slist[2])
+    {
+        car = 1;
+    }
+    else if(dlist[0]>=slist[1] && dlist[1]>slist[2])
+    {
+        car = 2 ;
+    }
+
+    printf("%d %d %d %d %d %d\n",slist[0],dlist[0],slist[1],dlist[1],slist[2],dlist[2]);
     printf("%d",car);
 }
