8a80af089ab6fa47a50f7d6b0318592298bbe54b
Bigcat34 <146065179+Bigcat34@users.noreply.github.com>
2023-10-23 19:12:36 +0800

    Update hw3-2.c

--- hw3-2.c
+++ hw3-2.c
@@ -1,30 +1,43 @@
 #include <stdio.h>
-int main() {
-    int s1, d1, s2, d2, s3, d3;
-
-    // 輸入三筆訂單的出發時間和返回時間
-    //printf("請輸入三筆訂單的出發時間和返回時間，格式為s1 d1 s2 d2 s3 d3：");
-    scanf("%d %d %d %d %d %d", &s1, &d1, &s2, &d2, &s3, &d3);
-
-    // 計算需要的遊覽車數量
-    int neededBuses ;
-
-    // 需要派3輛的情況
-    if ((d1!=s2 && d1!=s3) && (d2!=s1 && d2!=s3) && (d3!=s1 && d3!=s2) ) {
-        // 如果第二輛車的發車時間早於第一輛車的返回時間，則不需要額外的車
-        neededBuses = 3;
-    }
-
-    // 需要派1輛的情況
-    else if ((d1==s2 && d2==s3) ||(d1==s3 && d3==s2)||(d2==s1 && d1==s3) ||(d2==s3 && d3==s1)||(d3==s1 && d1==s2) ||(d3==s2 && d2==s1)) {
-
-        neededBuses = 1;
-    }
-    else
+main()
+{
+    int car = 3 ,s1,d1,s2,d2,s3,d3 ;
+    scanf("%d %d %d %d %d %d",&s1,&d1,&s2,&d2,&s3,&d3);
+    int slist[] = {s1,s2,s3};
+    int dlist[] = {d1,d2,d3};
+     for (int i = 0 ; i < 3 ; i++)
     {
-        neededBuses = 2;
+
+        if (car == 1)
+        {
+        break;
+        }
+
+        for (int j = 0 ; j < 3 ; j++)
+        {
+            if (car == 1)
+            {
+                break;
+            }
+            if (slist[j]>=dlist[i])
+            {
+                for (int k = 0 ; k < 3 ; k++)
+                {
+                   if (slist[k]>=dlist[j])
+                   {
+                       car = 1;
+                   }
+                    else
+                   {
+
+                       car = 2;
+                   }
+
+                }
+
+            }
+        }
+
     }
-    // 輸出結果
-    printf("%d", neededBuses);
-    return 0;
+    printf("%d",car);
 }
